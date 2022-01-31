8293c0a3182bf16ad9aa7e6b71a09d8dedb3039a
suyunann <suyuu8888@gmail.com>
2022-01-31 16:21:49 +0800

    flipper

--- Color_flipper/app.js
+++ Color_flipper/app.js
@@ -1,1 +1,7 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+// alert('歡迎來到 COLOR FLIPPER')
+ function colorFlip(){
+  let colordiv=Math.floor(Math.random()*colors.length);
+  document.body.style.background=colors[colordiv];
+  document.getElementById('color').innerHTML=colors[colordiv];
+} 
--- Color_flipper/hex.js
+++ Color_flipper/hex.js
@@ -1,1 +1,11 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+function colorFlip(){
+    let colorSym = "#";
+    for(let i = 0; i <= 5; i++){
+        let colorCombine = Math.floor(Math.random()*hex.length);
+        colorSym = colorSym + hex[colorCombine]; 
+    }
+    document.body.style.backgroundColor = colorSym;
+    document.getElementById('color').innerHTML = colorSym;
+    
+}
